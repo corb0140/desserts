3b8c46382909390b205bff707892fd6d7207d5d7
Mark Corbin <markpc1608@gmail.com>
2024-07-31 18:56:29 -0400

    order total update

--- app/components/Cart/Cart.js
+++ app/components/Cart/Cart.js
@@ -7,8 +7,20 @@ const Cart = ({ emptyCart, numberOfItems, items }) => {
   const [cartItems, setCartItems] = useState([]);
   const [total, setTotal] = useState(0);
 
+  const removeItem = (id) => {
+    const index = cartItems.findIndex((item) => item.id === id);
+    cartItems.splice(index, 1);
+    setCartItems([...cartItems]);
+
+    //remove item from cart and update total
+  };
+
   useEffect(() => {
     console.log(items);
+    const total = items.reduce((acc, item) => {
+      return acc + parseFloat(item.price);
+    }, 0);
+    setTotal(total);
     setCartItems(items);
   }, [items, setCartItems, cartItems]);
 
@@ -18,7 +30,7 @@ const Cart = ({ emptyCart, numberOfItems, items }) => {
       lg:fixed lg:top-0 lg:right-24 lg:w-[24rem]"
     >
       <h2 className="text-2xl text-primary font-bold">
-        Your Cart {`(${numberOfItems})`}
+        Your Cart {`(${cartItems.length === 0 ? 0 : numberOfItems})`}
       </h2>
       {cartItems.length === 0 ? (
         <>
@@ -45,19 +57,20 @@ const Cart = ({ emptyCart, numberOfItems, items }) => {
                   <div className="flex items-center gap-x-5">
                     <span className="text-sm text-primary">1x</span>
                     <p className="text-sm text-gray-400">@ ${item.price}0</p>
-                    <p className="text-sm text-gray-400">${total}</p>
+                    <p className="text-sm text-gray-400">$0.00</p>
                   </div>
                 </div>
               </div>
 
               <div
-                className="flex items-center justify-center h-[1.2rem] w-[1.2rem] border-2 border-Rose400 rounded-full
-              hover:cursor-pointer"
+                className="flex items-center justify-center h-[1.2rem] w-[1.2rem] border-2 
+                border-Rose400 rounded-full hover:cursor-pointer"
               >
                 <Image
                   src={remove}
                   alt="icon for removing item"
                   className="font-bold"
+                  onClick={() => removeItem(item.id)}
                 />
               </div>
             </div>
@@ -71,7 +84,7 @@ const Cart = ({ emptyCart, numberOfItems, items }) => {
         <div className="flex flex-col gap-y-5">
           <div className="flex items-center justify-between">
             <p className="text-md text-Rose900">Order Total</p>
-            <p className="text-3xl font-bold text-Rose900">$100.00</p>
+            <p className="text-3xl font-bold text-Rose900">${total}0</p>
           </div>
 
           <div className="h-12 w-full bg-Rose100 rounded-lg flex justify-center items-center gap-x-2">
@@ -82,7 +95,7 @@ const Cart = ({ emptyCart, numberOfItems, items }) => {
               style={{ objectFit: "contain" }}
             />
             <p className="text-sm text-Rose900">
-              This is a <span className="font-bold">carbon-neutral</span>{" "}
+              This is a <span className="font-bold">carbon-neutral</span>
               delivery
             </p>
           </div>
